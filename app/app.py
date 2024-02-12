7b8f3d5ee0d1d41a749b1d5bd3c8447802d3a544
adriannole <fernan877@hotmail.com>
2024-02-12 16:47:17 -0500

    update

--- app/app.py
+++ app/app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return "hola"
+def Index():
+    return render_template('Index.html')
     
 if __name__ == '__main__':
     app.run(debug=True)
