bb512f096f7db11cd8489944e1d162d568734b41
adriannole <fernan877@hotmail.com>
2024-02-12 18:17:14 -0500

    Update

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -2,3 +2,13 @@ const btnSingIn = document.getElementById("sing-in"),
       btnSingUp = document.getElementById("sing-up"),
       FormRegister = document.querySelector(".register"),
       formLogin = document.querySelector(".login");
+btnSingIn.addEventListener("click", e => {
+    formRegister.classList.add("hide")
+    formLogin.classList.remove("hide")
+})
+
+btnSingUp.addEventListener("click", e => {
+    formLogin.classList.add("hide")
+    formRegister.classList.remove("hide")
+})
+    
