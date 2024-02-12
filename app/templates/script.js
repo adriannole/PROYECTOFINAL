0269c50080189c7feedcb94e2b194dbcad4e525a
adriannole <fernan877@hotmail.com>
2024-02-12 18:08:08 -0500

    update

---
+++ app/templates/script.js
@@ -0,0 +1,4 @@
+const btnSingIn = document.getElementById("sing-in"),
+      btnSingUp = document.getElementById("sing-up"),
+      FormRegister = document.querySelector(".register"),
+      formLogin = document.querySelector(".login");
