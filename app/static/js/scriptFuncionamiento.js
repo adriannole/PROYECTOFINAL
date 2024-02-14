e80018c8c9634c7e8e901cd383ce0fbd3d2b7c83
adriannole <fernan877@hotmail.com>
2024-02-13 19:37:22 -0500

    update
    Actualizacion de login, pagina de presentacion y funcionalidades

--- app/static/js/scriptFuncionamiento.js
+++ app/static/js/scriptFuncionamiento.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", function() {
-  const animatedContainer = document.querySelector('.animated-container');
-  animatedContainer.classList.add('show');
+  const texts = document.querySelectorAll('.text');
+  let currentIndex = 0;
+  let firstChange = true;
+
+  function showText(index) {
+      texts.forEach((text, i) => {
+          if (i === index) {
+              text.classList.add('active');
+              speakText(text.innerText); // Llama a la función para leer el texto en voz alta
+          } else {
+              text.classList.remove('active');
+          }
+      });
+  }
+
+  function toggleText() {
+      if (firstChange) {
+          currentIndex++;
+          firstChange = false;
+      } else {
+          currentIndex = (currentIndex + 1) % texts.length;
+      }
+      showText(currentIndex);
+  }
+
+  // Función para leer el texto en voz alta
+  function speakText(text) {
+      const speech = new SpeechSynthesisUtterance(text);
+      speech.lang = 'es-ES'; // Establece el idioma del texto a español
+      window.speechSynthesis.speak(speech);
+  }
+
+  // Mostrar el primer texto al cargar la página
+  showText(currentIndex);
+
+  // Cambiar automáticamente el texto cada 3 segundos después del primer cambio
+  setInterval(() => {
+      toggleText();
+  }, 5000);
 });
